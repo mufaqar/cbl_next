6e790c9a4526c810830fb26d8392ef7aa727212c
mufaqar <mufaqar@gmail.com>
2023-09-04 20:01:29 +0500

    Search form

--- src/components/searchform.tsx
+++ src/components/searchform.tsx
@@ -33,61 +33,62 @@ query zones($zipcode: String = "") {
 
 
 const SearchForm = () => {
-    const [zipcode, setzipcode] = useState<string>();
-    const [pro_type, setpro_type] = useState<string>();
-    const router = useRouter();
-     const [loader, setloader] = useState<boolean>(false);
+  const [zipcode, setzipcode] = useState<string>();
+  const [pro_type, setpro_type] = useState<string>();
+  const router = useRouter();
+  const [loader, setloader] = useState<boolean>(false);
 
- 
-    function handleState() {     
- 
-      console.log(zipcode);
-       const variables = {
-        zipcode: zipcode // Replace with the actual user ID
-      };
-     
- 
-       async function fetchData() {
-           setloader(true);        
-          const response = await fetch('http://cblproject.aspactglobal.com/graphql', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ query , variables: variables }),
-          });
-          const respons = await response.json(); 
-          
-          if(respons){
-            setloader(false); 
-            router.push(`/${respons.data.zones.nodes[0].states.nodes[0].slug}/${respons.data.zones.nodes[0].cities.nodes[0].slug}?zipcode=${zipcode}&type=tv`);
-          }
-          else{
 
-            setloader(false); 
-          }
-        
+  function handleState() {
 
-        }
-        fetchData();      
-       // console.log("🚀  file: test.jsx:43  fetchData ~ data:", get_state); 
- 
-    
+    console.log(zipcode);
+    const variables = {
+      zipcode: zipcode // Replace with the actual user ID
+    };
 
-       }
 
-    return (
-        <>
-        {loader? <div className='fixed z-50 inset-0 bg-black/60 flex items-center flex-col justify-center'><div className="custom-loader"></div></div> :
-        
-            <div className="relative flex items-center w-full sm:px-12 px-6 mt-6 md:mt-10">
-                <FaMagnifyingGlass className="absolute ml-3" />
-                <input type="text" placeholder="Enter Zip Code" name="zip_code" value={zipcode} onChange={(e) => setzipcode(e.target.value)} className="w-full py-3 pl-10 pr-8 border outline-none md:w-80 border-zinc-400 rounded-l-md" />
-                <button className="px-4 py-[13px] font-semibold text-white bg-[#ef9831] border-[#ef9831] rounded-r-md" onClick={handleState}>Search</button>
-            </div>
-            }
-        </>
-    )
+    async function fetchData() {
+      setloader(true);
+      const response = await fetch('http://cblproject.aspactglobal.com/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Security-Policy': 'upgrade-insecure-requests'
+        },
+        body: JSON.stringify({ query, variables: variables }),
+      });
+      const respons = await response.json();
+
+      if (respons) {
+        setloader(false);
+        router.push(`/${respons.data.zones.nodes[0].states.nodes[0].slug}/${respons.data.zones.nodes[0].cities.nodes[0].slug}?zipcode=${zipcode}&type=tv`);
+      }
+      else {
+
+        setloader(false);
+      }
+
+
+    }
+    fetchData();
+    // console.log("🚀  file: test.jsx:43  fetchData ~ data:", get_state); 
+
+
+
+  }
+
+  return (
+    <>
+      {loader ? <div className='fixed z-50 inset-0 bg-black/60 flex items-center flex-col justify-center'><div className="custom-loader"></div></div> :
+
+        <div className="relative flex items-center w-full sm:px-12 px-6 mt-6 md:mt-10">
+          <FaMagnifyingGlass className="absolute ml-3" />
+          <input type="text" placeholder="Enter Zip Code" name="zip_code" value={zipcode} onChange={(e) => setzipcode(e.target.value)} className="w-full py-3 pl-10 pr-8 border outline-none md:w-80 border-zinc-400 rounded-l-md" />
+          <button className="px-4 py-[13px] font-semibold text-white bg-[#ef9831] border-[#ef9831] rounded-r-md" onClick={handleState}>Search</button>
+        </div>
+      }
+    </>
+  )
 }
 
 export default SearchForm
