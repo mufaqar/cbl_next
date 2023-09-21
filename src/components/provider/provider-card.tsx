6c716c8e37287109180b607a92f087a65300fbe7
mufaqar <mufaqar@gmail.com>
2023-09-21 19:59:24 +0500

    update

--- src/components/provider/provider-card.tsx
+++ src/components/provider/provider-card.tsx
@@ -5,7 +5,7 @@ export const ProviderCard = ({ item, zone }: any) => {
     return (
         <>
             <div className="w-full h-auto  rounded-t-md rounded-b-md flex flex-col">
-                
+
                 <div className="md:w-full w-full grid md:grid-cols-5 grid-cols-1 dtable">
                     <div className="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
                         <Image src={item.logo} alt="Feature Image" width={140} height={50} />
@@ -14,7 +14,7 @@ export const ProviderCard = ({ item, zone }: any) => {
                         <div>
                             <h4 className="text-center md:text-base text-xs font-bold">{item?.summery.speed}</h4>
                             <p className="text-center md:text-xs text-xs"> Mbps </p>
-                            <p className="text-center md:text-xs text-xs">{ item?.summery?.summaryChannel && <>{item?.summery?.summaryChannel} Channels</> }</p>
+                            <p className="text-center md:text-xs text-xs">{item?.summery?.summaryChannel && <>{item?.summery?.summaryChannel} Channels</>}</p>
                         </div>
                     </div>
                     <div className="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5 px-3">
@@ -31,20 +31,15 @@ export const ProviderCard = ({ item, zone }: any) => {
                                     </li>
                                 ))
                             }
-
-
                         </ul>
                     </div>
                     <div className="md:border-r border-r-0 md:border-b-0 border-b grid items-center justify-center p-5">
                         <div>
                             <h5 className="text-center md:text-base text-xs font-bold"> ${item?.price} /mo. </h5>
-                          
-                            
-                           
                         </div>
                     </div>
                     <div className="grid gap-3 items-center justify-center p-5">
-                        
+
                         <Link href={`/providers/${item.slug}`} className="text-base text-white font-[Roboto] uppercase px-5 py-2.5 bg-[#ef9831] hover:bg-[#215690]">View Plans</Link>
                     </div>
                 </div>
