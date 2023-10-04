eed0e43963740034f7dc5393b39818a6670b3c46
mufaqar <mufaqar@gmail.com>
2023-10-04 19:10:52 +0500

    sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -14,6 +14,8 @@ const query = `
 
 const SITE_URI = 'https://cablemovers.vercel.app';
 
+var date_time = new Date();
+
 async function fetchData() {
   const response = await fetch('https://cblproject.cablemovers.net/graphql', {
     method: 'POST',
@@ -27,6 +29,22 @@ async function fetchData() {
   return data;
 }
 
+function getCurrentDateInISO8601Format() {
+  const now = new Date();
+  const year = now.getUTCFullYear();
+  const month = String(now.getUTCMonth() + 1).padStart(2, '0'); // Month is zero-based, so add 1 and pad to two digits
+  const day = String(now.getUTCDate()).padStart(2, '0');
+  const hours = String(now.getUTCHours()).padStart(2, '0');
+  const minutes = String(now.getUTCMinutes()).padStart(2, '0');
+  const seconds = String(now.getUTCSeconds()).padStart(2, '0');
+  const iso8601Date = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}+00:00`;
+  return iso8601Date;
+}
+
+const currentDateInISO8601Format = getCurrentDateInISO8601Format();
+
+
+
 async function generateSitemap() {
   const { allProviders } = await fetchData();
 
@@ -34,17 +52,17 @@ async function generateSitemap() {
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
         <loc>${SITE_URI}</loc>
-        <lastmod>2023-05-11T19:00:11+00:00</lastmod>
+        <lastmod>${currentDateInISO8601Format}</lastmod>
         <priority>1.00</priority>
       </url>
       <url>
         <loc>${SITE_URI}/contact-us</loc>
-        <lastmod>2023-05-11T19:00:11+00:00</lastmod>
+        <lastmod>${currentDateInISO8601Format}</lastmod>
         <priority>1.00</priority>
       </url>
       <url>
         <loc>${SITE_URI}/about-us</loc>
-        <lastmod>2023-05-11T19:00:11+00:00</lastmod>
+        <lastmod>${currentDateInISO8601Format}</lastmod>
         <priority>1.00</priority>
       </url>
 
