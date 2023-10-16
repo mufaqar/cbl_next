94ebd37907d96c8038d57e1da99fd892acc772e5
mufaqar <mufaqar@gmail.com>
2023-10-16 19:12:27 +0500

    pages

--- src/pages/do-not-sell-my-information/index.tsx
+++ src/pages/do-not-sell-my-information/index.tsx
@@ -1,4 +1,5 @@
 import PageBanner from '@/components/pageBanner'
+import Link from 'next/link'
 import React from 'react'
 
 function Sell_Information() {
@@ -8,8 +9,8 @@ function Sell_Information() {
             <section className="py-16">
                 <div className='container mx-auto px-4'>
                     <div className="">
-                        <p className='text-xl mb-4'>
-                            If you wish to opt out of the sale of your personal information, please send an email to contact@cablemovers.net, including your first and last name and phone number. You will receive acknowledgment of your message within seven (7) business days. After confirming your identity, we will proceed with your request within thirty (30) days.
+                        <p className='text-xl mb-4 leading-relaxed'>
+                            If you wish to opt out of the sale of your personal information, please send an email to <Link href="mailto:contact@cablemovers.net">contact@cablemovers.net</Link>, including your first and last name and phone number. You will receive acknowledgment of your message within seven (7) business days. After confirming your identity, we will proceed with your request within thirty (30) days.
                         </p>
                     </div>
                 </div>
