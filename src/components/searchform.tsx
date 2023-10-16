0884cfe497b1b7352f5ca67dce7f08eb9e367b52
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-16 18:02:24 +0500

    search with enter

--- src/components/searchform.tsx
+++ src/components/searchform.tsx
@@ -39,46 +39,51 @@ const SearchForm = () => {
   const router2 = Router();
   const [loader, setloader] = useState<boolean>(false);
 
+  const variables = {
+    zipcode: zipcode
+  };
+  async function fetchData() {
+    setloader(true);
+    const response = await fetch('https://cblproject.cablemovers.net/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ query, variables: variables }),
+    });
+    const respons = await response.json();
+
+    if (respons) {
+      router.push(`/${respons.data?.zones?.nodes[0]?.states?.nodes[0]?.slug}/${respons.data.zones?.nodes[0]?.cities.nodes[0].slug}?zipcode=${zipcode}&type=internet`);
+      setTimeout(()=>{
+        setloader(false);
+      }, router2.pathname === "/" ? 2000 : 1000)
+    }
+    else {
+      setloader(false);
+    }
+  }
 
   function handleState() {
-    const variables = {
-      zipcode: zipcode
-    };
-
-
-    async function fetchData() {
-      setloader(true);
-      const response = await fetch('https://cblproject.cablemovers.net/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ query, variables: variables }),
-      });
-      const respons = await response.json();
-
-      if (respons) {
-        router.push(`/${respons.data?.zones?.nodes[0]?.states?.nodes[0]?.slug}/${respons.data.zones?.nodes[0]?.cities.nodes[0].slug}?zipcode=${zipcode}&type=internet`);
-        setTimeout(()=>{
-          setloader(false);
-        }, router2.pathname === "/" ? 2000 : 1000)
-      }
-      else {
-        setloader(false);
-      }
-    }
     fetchData();
+  }
+
+  const handleKeyDown = (event:any) => {
+    if (event.key === 'Enter') {
+      fetchData()
+    }
   }
 
   return (
     <>
       {loader ? <div className='fixed z-50 inset-0 bg-black/60 flex items-center flex-col justify-center'><div className="custom-loader"></div></div> :
-
+      <form>
         <div className="relative flex items-center w-full m-auto serch_form">
           <FaMagnifyingGlass className="absolute ml-3" />
-          <input type="text" placeholder="Enter Zip Code" maxLength={5} name="zip_code" value={zipcode} onChange={(e) => setzipcode(e.target.value)} className="w-full py-3 pl-10 pr-8 border outline-none md:w-80 border-zinc-400 rounded-l-md" />
+          <input type="text" onKeyDown={handleKeyDown} placeholder="Enter Zip Code" maxLength={5} name="zip_code" value={zipcode} onChange={(e) => setzipcode(e.target.value)} className="w-full py-3 pl-10 pr-8 border outline-none md:w-80 border-zinc-400 rounded-l-md" />
           <button className="px-4 py-[13px] font-semibold text-white bg-[#ef9831] border-[#ef9831] rounded-r-md" onClick={handleState}>Search</button>
         </div>
+        </form>
       }
     </>
   )
