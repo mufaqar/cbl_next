c669eb9aa6bf8983e5db8bc0739fedc8bd978dbc
mufaqar <mufaqar@gmail.com>
2023-10-16 20:19:16 +0500

    favicon

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -9,26 +9,21 @@ import { useRouter } from 'next/router'
 import React, { useContext } from 'react'
 
 const Category = ({ SinglePost }: any) => {
- console.log("🚀 ~ file: [slug].tsx:10 ~ Category ~ posts:", SinglePost)
- 
-  
+  console.log("🚀 ~ file: [slug].tsx:10 ~ Category ~ posts:", SinglePost)
+
+
 
   return (
     <>
-           <PageBanner title={SinglePost.title} />
-           <main className="py-16 max-w-screen-lg mx-auto">
-      
-     
-      <div className="p-4">
-            <Image src= {SinglePost?.featuredImage?.node?.mediaItemUrl} alt="technology" className="rounded-lg" width={1280} height={853} />
-            <div className="p-5">
-              
-                <div className="text-gray-700 mt-2 text-justify text-sm" dangerouslySetInnerHTML={{ __html: SinglePost?.content }} />
-            </div>
+      <PageBanner title={SinglePost.title} />
+      <main className="py-16 max-w-screen-lg mx-auto">
+        <div className="p-4">
+          <Image src={SinglePost?.featuredImage?.node?.mediaItemUrl} alt="technology" className="rounded-lg" width={1280} height={853} />
+          <div className="p-5">
+            <div className="text-gray-700 mt-2 text-justify text-sm" dangerouslySetInnerHTML={{ __html: SinglePost?.content }} />
+          </div>
         </div>
       </main>
-
-    
     </>
   )
 }
