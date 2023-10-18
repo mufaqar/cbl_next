fe7555a7fb52e6f554b03b1c3f286b5bbcc12925
mufaqar <mufaqar@gmail.com>
2023-10-18 20:48:46 +0500

    phone

--- src/components/provider/provider-card.tsx
+++ src/components/provider/provider-card.tsx
@@ -75,10 +75,7 @@ export const ProviderCard = ({ item, type, offer , count }: any) => {
                             </p>
                         </div>
                     </div>
-                    <div className="grid gap-3 items-center justify-center p-5">
-                        <Link href="tel:000-000-000" className="text-base text-center font-medium text-white bg-[#ef9831] hover:bg-[#215690] px-3 py-1.5 rounded-3xl">
-                            {item?.mobileNo}
-                        </Link>
+                    <div className="grid gap-3 items-center justify-center p-5">                        
                         <Link href={`/providers/${item.slug}`} className="text-base text-white font-[Roboto] uppercase px-5 py-2.5 bg-[#ef9831] hover:bg-[#215690]">View Plans</Link>
                     </div>
                 </div>
