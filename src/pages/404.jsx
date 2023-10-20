ddbeb84e62207932d031a9b1970424329131ee48
syed zunair gillani <zunairgillani54@gmail.com>
2023-10-20 16:26:25 +0500

    update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,33 +1,7 @@
 import Link from 'next/link'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 const NotFound = () => {
-     const allBlogs = `
-     query allBlogs {
-       posts(first: 100000) {
-         nodes {
-           date
-           slug
-         }
-       }    
-     }
-   `;
-     useEffect(()=>{
-          async function fetchBlogs() {
-               const responseForBlogs = await fetch('https://cblproject.cablemovers.net/graphql', {
-                 method: 'POST',
-                 headers: {
-                   'Content-Type': 'application/json',
-                 },
-                 body: JSON.stringify({ allBlogs }),
-               });
-               const { data } = await responseForBlogs.json();
-               return data;
-             }
-             const blogslist = await fetchBlogs()
-             console.log('blogslist', blogslist)
-     },[])
-
      return (
           <section>
                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
