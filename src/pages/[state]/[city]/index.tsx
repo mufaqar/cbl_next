73cb7c6d07485859b91258e13b19db048e7de107
mufaqar <mufaqar@gmail.com>
2023-10-24 14:20:56 +0500

    city data

--- src/pages/[state]/[city]/index.tsx
+++ src/pages/[state]/[city]/index.tsx
@@ -46,7 +46,7 @@ export default function Providers({ allProviders, zones, zipcode, my_city, provi
 
 
   return (
-    zipcode ? <Zip_Code_Com zipcode={zipcode} city={city} state={state} allProviders={allProviders} zones={zones} type='internet' /> : <Cities_com city={city}  my_city={my_city} city_data={city_data} allProviders={allProviders} type="internet" />
+    zipcode ? <Zip_Code_Com zipcode={zipcode} city={city} state={state} allProviders={allProviders} zones={zones} type='internet' /> : <Cities_com city={city}  my_city={my_city} allProviders={allProviders} type="internet" />
 
   );
 }
