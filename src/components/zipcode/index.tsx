84b0a4ee2cfeb2340e761b00207116ebc24011fb
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2023-11-13 18:13:26 -0500

    Update index.tsx

--- src/components/zipcode/index.tsx
+++ src/components/zipcode/index.tsx
@@ -24,7 +24,7 @@ function Zip_Code_Com({ zipcode, city, state, allProviders, zones }: any) {
 
     const { query } = useRouter();
     const city_code = query.city;
-    const state_code = query.state;
+    const state = query.state;
     let C_State = (state as string).toUpperCase();
     var type = query?.type;
     var types = query?.type;
