515f584f19c9064e1d02c0791cdf4555dad3d491
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2023-11-13 17:17:22 -0500

    Update faqs_city.tsx

--- src/components/faqs_city.tsx
+++ src/components/faqs_city.tsx
@@ -328,7 +328,7 @@ export default function Faqs_City({ city, type, allProviders, totalProviderCount
                             <p className="text-base font-medium mt-5" >
                                 {countServiceType}  bundle service providers are available in {city}. Based on the availability  {
                                     allProviders[0]?.title
-                                }  is the best bundle service provider in {city}, <span className="uppercase"{state} </span>.
+                                }  is the best bundle service provider in <span className=" uppercase">{city} {state} </span>.
                             </p>
                         </div>
                     </div>
