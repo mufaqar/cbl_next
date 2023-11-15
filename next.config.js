969c46143b1d987b0c1aceef8f9a2e6e919f2d51
mufaqar <mufaqar@gmail.com>
2023-11-15 15:40:09 +0500

    Redirect

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images:{
-    domains:['cblproject.cablemovers.net', 'localhost']
-  }
-}
 
-module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      { source: 'https://cablemovers.net', destination: 'https://www.cablemovers.net', permanent: true },
+      { source: 'http://cablemovers.net', destination: 'https://www.cablemovers.net', permanent: true },
+      { source: 'https://www.cablemovers.net/order-internet-and-tv-service', destination: 'https://www.cablemovers.net', permanent: true },
+      { source: 'http://www.cablemovers.net/order-internet-and-tv-service/', destination: 'https://www.cablemovers.net', permanent: true },
+      { source: 'https://www.cablemovers.net/feed/', destination: 'https://www.cablemovers.net', permanent: true },
+    
+
+    ];
+  },
+  reactStrictMode: true,
+  images: {
+    domains: ['cblproject.cablemovers.net', 'localhost'],
+  },
+};
