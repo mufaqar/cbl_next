f57b2dd15b662ab7ad63b1e0b319cf401a6f44af
mufaqar <mufaqar@gmail.com>
2023-11-15 17:25:07 +0500

    Redirectoin

--- next.config.js
+++ next.config.js
@@ -4,12 +4,10 @@
 module.exports = {
   async redirects() {
     return [
-      { source: 'https://cablemovers.net', destination: 'https://www.cablemovers.net', permanent: true },
-      { source: 'http://cablemovers.net', destination: 'https://www.cablemovers.net', permanent: true },
-      { source: 'https://www.cablemovers.net/order-internet-and-tv-service', destination: 'https://www.cablemovers.net', permanent: true },
-      { source: 'http://www.cablemovers.net/order-internet-and-tv-service/', destination: 'https://www.cablemovers.net', permanent: true },
-      { source: 'https://www.cablemovers.net/feed/', destination: 'https://www.cablemovers.net', permanent: true },
-    
+   
+      { source: '/order-internet-and-tv-service', destination: '/', permanent: true },
+      { source: '/order-internet-and-tv-service/', destination: '/', permanent: true },
+      { source: '/feed/', destination: '/', permanent: true } 
 
     ];
   },
