9818f5c844f883a3c05e2125a3bfc0d925cb5b48
mufaqar <mufaqar@gmail.com>
2023-11-16 11:39:36 +0500

    config updates N

--- next.config.js
+++ next.config.js
@@ -1,20 +1,25 @@
 /** @type {import('next').NextConfig} */
 
-
 module.exports = {
   async redirects() {
     return [
-   
-      { source: '/order-internet-and-tv-service', destination: '/', permanent: true },
-      { source: '/order-internet-and-tv-service/', destination: '/', permanent: true },
-      { source: '/feed', destination: '/', permanent: true } ,
-      { source: '/feed/', destination: '/', permanent: true } 
       {
-        source: 'http://www.cablemovers.net/:path*', // replace with your domain
+        source: '/order-internet-and-tv-service',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/order-internet-and-tv-service/',
+        destination: '/',
+        permanent: true,
+      },
+      { source: '/feed', destination: '/', permanent: true },
+      { source: '/feed/', destination: '/', permanent: true },
+      {
+        source: 'http://www.cablemovers.net/:path*',
         destination: 'https://www.cablemovers.net/:path*',
         permanent: true,
       },
-
     ];
   },
   reactStrictMode: true,
