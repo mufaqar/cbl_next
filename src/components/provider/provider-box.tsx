93ba1442005c8abddd7a5a3aac69ee4d4a812133
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2023-12-06 11:56:07 -0500

    Update provider-box.tsx

--- src/components/provider/provider-box.tsx
+++ src/components/provider/provider-box.tsx
@@ -7,7 +7,7 @@ export const ProviderBox = ({ item }: any) => {
 
             <div className="block rounded-xl border border-gray-100 p-3 shadow-xl transition hover:border-[#215690]/10 hover:shadow-[#215690]/10">
                 <Link href={`/providers/${item.slug}`} >
-                    <Image src={item.featuredImage?.node.mediaItemUrl} alt="Feature" width={140} height={50} className='mx-auto' />
+                    <Image src={item.featuredImage?.node.mediaItemUrl} alt="Partners logos by Cable Movers" width={140} height={50} className='mx-auto' />
                     <h2 className="mt-4 text-lg  text-center uppercase">
                         {item.title}
                     </h2>
