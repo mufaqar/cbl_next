93c7d604c8ca30dd0a9f45554fbcce93a10cc1fa
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2023-12-21 17:45:23 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,8 +9,6 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <GoogleAnalytics measurementId="G-Q1VGHRVGLX" />
       <script src=//b-js.ringba.com/CA7b594600650445d183a4f303631553d4 async></script>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-396439267"></script>
-      <script> gtag('event', 'conversion', {'send_to': 'AW-396439267/vIIYCOHA6IAZEOPdhL0B'});</script>
       <Header />
       <Component {...pageProps} />
       <Footer />
