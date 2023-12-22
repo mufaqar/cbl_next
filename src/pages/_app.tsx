6d2561cfcade11479a6e8283e6b12a7b35abf3fa
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2023-12-22 10:29:02 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics measurementId="G-Q1VGHRVGLX" />
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-396439267"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-396439267');
+</script>
       <Header />
       <Component {...pageProps} />
       <Footer />
