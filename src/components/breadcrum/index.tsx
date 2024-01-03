37323fdf74fc7a74d685750f997b49a9b164cb0f
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2024-01-03 10:41:02 -0500

    Update index.tsx

--- src/components/breadcrum/index.tsx
+++ src/components/breadcrum/index.tsx
@@ -20,7 +20,7 @@ const Breadcrum = () => {
                     {
                          bredcrumList?.map((item:any, idx:number) => (
                               <li className="inline-flex items-center" key={idx}>
-                                   <Link className={`flex cursor-pointer items-center text-sm  ${idx+1 === bredcrumList.length ? '!text-gray-300' : 'text-gray-500 hover:text-blue-600 focus:outline-none'}`} href={`/${item}`}>
+                                   <Link className={`flex cursor-pointer items-center text-sm  ${idx+1 === bredcrumList.length ? '!text-gray-300' : 'text-gray-500 hover:text-blue-600 focus:outline-none'}`} href={`/${item.slug}`}>
                                         {item?.name}
                                         {
                                              idx+1 !== bredcrumList.length && <svg className="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m9 18 6-6-6-6" /></svg>
