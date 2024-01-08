c4b08750c956e05612c7d841d97a2c1a136c1fe7
mufaqar <mufaqar@gmail.com>
2024-01-08 18:51:35 +0500

    Scritps

---
+++ public/script.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'AW-396439267');
+
+
+gtag('event', 'conversion', {'send_to': 'AW-396439267/vIIYCOHA6IAZEOPdhL0B'});
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-396439267"></script>
+        <script async src="../../public/script.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <script async src="../../public/footer-script.js" />
       </body>
     </Html>
   )
