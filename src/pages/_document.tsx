add98fbac9f981f9412e11a1f2c81a27b9898aae
mufaqar <mufaqar@gmail.com>
2024-01-08 18:55:32 +0500

    next scritps

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-396439267"></script>
-        <script async src="../../public/script.js" />
+        <script async src="/script.js" />
       </Head>
       <body>
         <Main />
         <NextScript />
-        <script async src="../../public/footer-script.js" />
+        <script async src="/footer-script.js" />
       </body>
     </Html>
   )
