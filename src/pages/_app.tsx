481095b8d5aa951dc2228936adcdfcccd7c562bc
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2024-01-09 10:09:20 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics measurementId="AW-396439267" />
+      <GoogleAnalytics measurementId="G-Q1VGHRVGLX" />
       <Header />
       <ApolloProvider client={apolloClient}>
       <Component {...pageProps} />
