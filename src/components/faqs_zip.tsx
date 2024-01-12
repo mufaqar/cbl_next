5656e3c792adee8f8d3a3108b47ba38769879fe6
Rehman Malik <140843890+rmlk1@users.noreply.github.com>
2024-01-12 12:33:01 -0500

    Update faqs_zip.tsx

--- src/components/faqs_zip.tsx
+++ src/components/faqs_zip.tsx
@@ -251,7 +251,7 @@ export default function Faqs_Zip({ zipcode, city, type, allProviders, totalProvi
             {type === "internet-tv" &&
                 <>
                     <h2 className="text-2xl font-bold">
-                        <span className=" uppercase"> <span className="  text-[#ef9831] uppercase">{city} {state} </span> </span>  Internet and TV FAQ’s </h2>
+                        <span className=" uppercase"> <span className="  text-[#ef9831] uppercase">{city} {state} </span> </span>  TV and Internet FAQ’; </h2>
 
                     <div className="w-full h-fit border border-[#F0F0F0] rounded-[10px] p-[30px] shadow-[0_15px_15px_rgba(0,0,0,0.05)]">
                         <div className="">
