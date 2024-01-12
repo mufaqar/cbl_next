ec961d379069dc36d7507ebe8b043e4991802a62
mufaqar <mufaqar@gmail.com>
2024-01-12 21:34:37 +0500

    G Tag Updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-396439267"></script>
+        <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-TNZF6NMR"></script>
         <script async src="/script.js" />
       </Head>
       <body>
@@ -16,7 +16,7 @@ export default function Document() {
         />
         <Main />
         <NextScript />
-        <script async src="/footer-script.js" />
+        {/* <script async src="/footer-script.js" /> */}
       </body>
     </Html>
   )
