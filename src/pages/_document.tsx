ea137701401199daca93373de2faa151a36a6a6b
mufaqar <mufaqar@gmail.com>
2024-01-12 21:42:19 +0500

    GM@

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-TNZF6NMR"></script>
+        {/* <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-TNZF6NMR"></script> */}
         <script async src="/script.js" />
       </Head>
       <body>
